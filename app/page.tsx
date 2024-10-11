fb1792dcaa16eb36bb9b3a5d94503ef835b09222
Catherine Siller <cathysiller503@gmail.com>
2024-10-10 22:02:40 -0500

    fix lint issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import { Form } from "@/components/form";
-import { title, subtitle } from "@/components/primitives";
+import { title } from "@/components/primitives";
 
 export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="inline-block max-w-xl text-center justify-center pb-24">
-        <span className={title()}>Let's&nbsp;</span>
+        <span className={title()}>Let&apos;s&nbsp;</span>
         <span className={title({ color: "violet" })}>solve&nbsp;</span>
         <span className={title()}>your</span>
         <br />
